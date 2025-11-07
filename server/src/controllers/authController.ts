98d26d6b1401e927c36be18d1b0154cffe99e08f
frezer takele <129789211+fretak1@users.noreply.github.com>
2025-11-07 21:34:26 +0300

    Update authController.ts

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -160,7 +160,7 @@ async function setTokens(
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    sameSite: "none",
     path: "/",
     maxAge: 60 * 60 * 1000,
   });
@@ -168,7 +168,7 @@ async function setTokens(
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    sameSite: "none",
     path: "/",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
