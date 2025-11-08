8fce4293ca657a66b723d3b9c96d3506cabc2ccf
Fretak <frezertakele1@gmail.com>
2025-11-08 12:17:04 +0300

    Add resend package and update sendEmail.ts

--- server/src/utils/sendEmail.ts
+++ server/src/utils/sendEmail.ts
@@ -1,22 +1,12 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (to: string, subject: string, html: string) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail", // or any other email service
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
-    });
-
-    await transporter.sendMail({
-      from: `"Ethio Market" <${process.env.EMAIL_USER}>`,
-      to, // single email at a time
-      subject,
-      html,
-    });
-  } catch (error) {
-    console.error(`Failed to send email to ${to}:`, error);
-  }
+  await resend.emails.send({
+    from: `"Ethio Market" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html,
+  });
 };
